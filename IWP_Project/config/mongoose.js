501016a43885b0ae85e80dcedf9a3c8396c818f3
Kartikay77 <73048872+Kartikay77@users.noreply.github.com>
2021-01-02 21:02:46 +0530

    Delete mongoose.js

--- IWP_Project/config/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://127.0.0.1:27017/an1', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: true,
-    useUnifiedTopology: true,
-    
-});
